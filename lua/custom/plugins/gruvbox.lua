8430a428acafb8cac0acce9cbad9d3e3318aa883
Nick Rakoczy <nickrak@icloud.com>
2025-06-28 12:33:58 -0600

    Fix transparency on gruvbox

--- lua/custom/plugins/gruvbox.lua
+++ lua/custom/plugins/gruvbox.lua
@@ -2,6 +2,11 @@ return {
   {
     'ellisonleao/gruvbox.nvim',
     priority = 1000,
+    config = true,
+    opts = {
+      terminal_colors = true,
+      transparent_mode = true,
+    },
     init = function()
       vim.cmd.colorscheme 'gruvbox'
       vim.o.background = 'dark'
